e972d89ce6dc124161e4770cfb488bdda8fb5ff4
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-10-26 14:58:41 +0100

    Simulate validation of the token by the database

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from pyramid.config import Configurator
 from pyramid_jwtauth import includeme
 
 from pyramid.security import Allow, Everyone
+from pyramid.httpexceptions import HTTPUnauthorized
 
 class RootFactory(object):
     __name__ = 'RootFactory'
@@ -12,11 +13,38 @@ class RootFactory(object):
     def __init__(self, request):
         pass
 
+def jwt_database_validation_tween_factory(handler, registry):
+    # Check the validity of the received token
+    # Should be run after the cookie to authorization tween.
+
+    def tween(request):
+        if request.authorization is None:
+            # Skipping validation if there is no authorization object.
+            # This is dangerous since a bad ordering of this tween and the
+            # cookie tween would bypass security
+            return handler(request)
+        else:
+            # Skip the token=" prefix and " suffix
+            token = request.authorization[1][7:-1]
+            print(token)
+    
+            # Simulate database interaction by requiring token to end with Q
+            valid = token.endswith("Q")
+    
+            if valid:
+                return handler(request)
+            else:
+                return HTTPUnauthorized("Bad token") # TODO: clear cookie?
+
+    return tween
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+
+    config.add_tween('api.jwt_database_validation_tween_factory')
 
     config.include("pyramid_jwtauth")
     config.set_root_factory(RootFactory)
